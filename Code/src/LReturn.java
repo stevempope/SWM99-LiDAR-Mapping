bf0c49d74cc33ef21ab2f89e8b5c8fc016194999
Stephen Pope <stevempope@gmail.com>
2019-02-15 17:09:57 +0000

    Comments for LReturn
    Added JavaDoc comments for LReturn

--- Code/src/LReturn.java
+++ Code/src/LReturn.java
@@ -2,77 +2,167 @@ package lidarMapping;
 
 import java.util.ArrayList;
 
+/**
+ * An LReturn represents the presence of a single blockage detected in a LiDAR Return.
+ * Multiple LReturns combine to create a ReturnSet.
+ * An LReturn contains the starting angle the block was detected at, a list of distances
+ * in order pertaining to that block (starting with the starting angle and sequentially
+ * recording space) and finally the ending angle that block occurs at.
+ * 
+ * For Pathfinding purposes, each blockage also holds two scores, corresponding to its calculated
+ * values from the latest run of the pathfinding algorithm.
+ * 
+ * @author Stephen Pope 15836791
+ * @version 0.1
+ *
+ */
+
 public class LReturn {
 	private Integer startAngle;
 	private Integer startScore;
 	private Integer endAngle;
 	private Integer endScore;
 	private ArrayList<Integer> returnList;
-	
+
+	/**
+	 *The blank constructor for an LReturn. Can be used in conjunction with the setter methods
+	 *to set incoming values from the VLSensor and Processor	
+	 */
 	public LReturn () {
 		returnList = new ArrayList<Integer>();
 	}
-	
+
+	/**
+	 * As above but with arguments to get the object started. Normally this is the
+	 * constructor we would use
+	 * @param start - The starting angle of the blockage
+	 * @param blockage - The distance the start of the blockage is away
+	 */
 	public LReturn (Integer start, Integer blockage) {
 		startAngle = start;
 		returnList = new ArrayList<Integer>();
 	}
-	
+
+	/**
+	 * As above but a full arrayList is provided. Used for copying an LReturn
+	 * Potentially useful for map rotation later
+	 * @param start - The starting angle of the blockage
+	 * @param blocks - The ArrayList of Blockage distances
+	 * @param end - The ending angle of the blockage
+	 */
 	public LReturn (Integer start, ArrayList<Integer> blocks, Integer end) {
 		startAngle = start;
 		returnList = blocks;
 		endAngle = end;
 	}
-	
+
+	/**
+	 * Gets the starting angle
+	 * @return the starting angle
+	 */
 	public Integer getStart() {
 		return startAngle;
 	}
-	
+
+	/**
+	 * Sets the starting angles value to the provided value
+	 * @param start - the starting angle of a blockage
+	 * @return true
+	 */
 	public boolean setStart(Integer start) {
 		startAngle = start;
 		return true;
 	}
-	
+
+	/**
+	 * Gets the ending angle
+	 * @return the ending angle
+	 */
 	public Integer getEnd() {
 		return endAngle;
 	}
-	
+
+	/**
+	 * Sets the ending angles value to the provided value
+	 * @param end
+	 * @return true
+	 */
 	public boolean setEnd(Integer end) {
 		endAngle = end;
 		return true;
 	}
-	
+
+	/**
+	 * Gets a distance from within the blockage at a specified angle
+	 * @param theAngle - the selected angle
+	 * @return the distance of the blockage at the given angle
+	 */
 	public Integer getDistance(Integer theAngle) {
 		return returnList.get(theAngle);
 	}
-	
+
+	/**
+	 * Adds a new distance on the end of the blockage list
+	 * @param theDistance - new distance in mm
+	 * @return true
+	 */
 	public boolean setDistance(Integer theDistance) {
 		returnList.add(theDistance);
 		return true;
 	}
-	
+
+	/**
+	 * Returns the calculated pathfinding score for the starting angle
+	 * @return the score
+	 */
 	public Integer getStartScore() {
 		return startScore;
 	}
 	
+	/**
+	 * Used by the pathfinding algorithm to set the score for the starting angle
+	 * of the blockage
+	 * @param score - the calculated score
+	 * @return true
+	 */
 	public boolean setStartScore(Integer score) {
 		startScore = score;
 		return true;
 	}
 	
+	/**
+	 * Gets the calculated pathfinding score for the end of the blockage
+	 * @return the score
+	 */
 	public Integer getEndScore() {
 		return endScore;
 	}
-
+	
+	/**
+	 * Used by the pathfinding algorithm to set the score for the ending angle
+	 * of the blockage
+	 * @param score - the calculated score
+	 * @return true
+	 */
 	public boolean setEndScore(Integer score) {
 		endScore = score;
 		return true;
 	}
 	
+	/**
+	 * Gets the distance associated with the starting angle (the first in the ArrayList
+	 * this cleans up the code to get the first item as it will be more commonly used)
+	 * @return the distance to the blockage at the start angle
+	 */
 	public Integer getStartDist() {
 		return returnList.get(0);
 	}
-	
+
+	/**
+	 * Gets the distance associated with the ending angle (the last in the ArrayList
+	 * this cleans up the code to get the last item as it will be more commonly used)
+	 * @return the distance to the blockage at the end angle
+	 */
 	public Integer getEndDist() {
 		return returnList.get(returnList.size() -1);
 	}
--- Code/src/Orientation.java
+++ Code/src/Orientation.java
@@ -1,5 +1,13 @@
 package lidarMapping;
 
+/**
+ * @author Stephen Pope 15836791
+ * Enumeration class for sensor abilities.
+ * In this build we assume that a LiDAR sensor operates in two dimensional space.
+ * 
+ * @version 0.1
+ */
+
 public enum Orientation {
 	clockwise, antiClockwise;
 }
