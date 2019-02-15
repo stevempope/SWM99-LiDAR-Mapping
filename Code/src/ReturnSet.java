e1f1df6faa494de920ccc61d7f7f0a2e147f1257
Stephen Pope <stevempope@gmail.com>
2019-02-15 17:20:36 +0000

    Comments for ReturnSet
    Added JavaDoc for a ReturnSet

--- Code/src/ReturnSet.java
+++ Code/src/ReturnSet.java
@@ -2,23 +2,52 @@ package lidarMapping;
 
 import java.util.ArrayList;
 
+/**
+ * A ReturnSet is a collection of LReturns that correspond to the output from a 
+ * single sense from the LiDAR sensor. This is used to distinguish from and allow
+ * input from multiple senses to be laid over each other or amalgamated.
+ * 
+ * ReturnSets are sensitve to orientation, meaning it is possible to have multiple
+ * sensors providing their own ReturnSets from different orientations and still have an operable
+ * map.
+ * 
+ * ReturnSets store an ArrayList of LReturns and the orientation of those returns.
+ * 
+ * @author Stephen Pope 15836791
+ * @version 0.1
+ */
+
 public class ReturnSet {
 	Orientation theOrientation;
 	ArrayList<LReturn> blockageList;
-	
+
+	/**
+	 * The base constructor for a ReturnSet. This assumes an anti-clockwise rotation.
+	 */
 	public ReturnSet() {
 		theOrientation = Orientation.antiClockwise;
 		blockageList = new ArrayList<LReturn>();
 	}
 	
+	/**
+	 * The regular constructor for a returnSet. Currently this only would be used when
+	 * a clockwise sensor is in use, but again when more sensor modes are added, this
+	 * becomes more relevant.
+	 * @param direction
+	 */
 	public ReturnSet (Orientation direction) {
 		theOrientation = direction;
 		blockageList = new ArrayList<LReturn>();
 	}
 
+	/**
+	 * Adds an LReturn (Blockage) to the list of blockages
+	 * @param block - The new LReturn
+	 * @return true
+	 */
 	public boolean addBlockage(LReturn block) {
 		blockageList.add(block);
 		return true;
 	}
-	
+
 }
